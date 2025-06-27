986ed91f8801cea7d193009f23236b2d5caaa917
YugyeongChoi <xyukyeong@naver.com>
2025-06-28 00:40:25 +0900

    chore:url 배포한 것으로 수정

--- src/main/frontend/src/setupProxy.js
+++ src/main/frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080', // 백엔드 주소
+            target: 'http://www.sejongglobalbuddy.kr', // 백엔드 주소
             changeOrigin: true,
         })
     );
