738137207d932cf5ab32bffb6d6f6737ef4cf52f
YugyeongChoi <xyukyeong@naver.com>
2025-07-10 15:56:19 +0900

    fix:PlaceholderResolutionException

--- src/main/java/sejong/globalbuddy/repository/ReviewRepository.java
+++ src/main/java/sejong/globalbuddy/repository/ReviewRepository.java
@@ -3,10 +3,13 @@ package sejong.globalbuddy.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import sejong.globalbuddy.entity.ReviewEntity;
 
 import java.util.Optional;
 
+
+@Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
     @Query("SELECT r FROM ReviewEntity r LEFT JOIN FETCH r.photos WHERE r.id = :id")
     Optional<ReviewEntity> findByIdWithPhotos(@Param("id") Long id);
