b3104ce85056e552782792f3f7ff65988045d435
YugyeongChoi <xyukyeong@naver.com>
2025-07-11 23:59:04 +0900

    fix:정적 리소스 캐시 무효화

--- src/main/java/sejong/globalbuddy/config/WebConfig.java
+++ src/main/java/sejong/globalbuddy/config/WebConfig.java
@@ -1,9 +1,12 @@
 package sejong.globalbuddy.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,5 +17,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .setViewName("forward:/");
         registry.addViewController("/**/{path:[^\\.]*}")
                 .setViewName("forward:/");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noStore());
     }
 }
