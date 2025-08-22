5d62e6befd56daa51b52f1bf377a18987701819d
YugyeongChoi <xyukyeong@naver.com>
2025-08-22 11:27:38 +0900

    fix:report에도 /api 추가

--- src/main/java/sejong/globalbuddy/controller/EmailReportController.java
+++ src/main/java/sejong/globalbuddy/controller/EmailReportController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/review")
+@RequestMapping("/api/review")
 public class EmailReportController {
 
     @Autowired
